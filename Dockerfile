3f0d372be03b58c77a6532781f7048cfa0cb8b79
Eric LeVin <eric@ericlevin.me>
2023-06-08 14:22:48 -0500

    Added custom grants retriever

--- Dockerfile
+++ Dockerfile
@@ -4,44 +4,24 @@ ARG KEYCLOAK_IMAGE="quay.io/keycloak/keycloak:20.0.2"
 FROM maven:3.8 as jdk-builder
 
 WORKDIR /workspace
-COPY . ./
+COPY ./protocol-mapper/ ./protocol-mapper/
+COPY ./pom.xml .
 
 RUN mvn clean package
+
+FROM quay.io/adorsys/keycloak-config-cli:latest as keycloak-config-cli
 
 # Build keycloak
 FROM ${KEYCLOAK_IMAGE} as keycloak-builder
 
 COPY --from=jdk-builder /workspace/protocol-mapper/target/keycloak-custom-protocol-mapper-example.jar /opt/keycloak/providers/
+COPY --from=keycloak-config-cli /app/keycloak-config-cli.jar /opt/keycloak-config-cli.jar
 
 RUN /opt/keycloak/bin/kc.sh build
 
 # Create keycloak image
 FROM ${KEYCLOAK_IMAGE}
 
-COPY --from=keycloak-builder /opt/keycloak/ /opt/keycloak/
-
 WORKDIR /opt/keycloak
-
-COPY ./docker-entrypoint.sh /opt/keycloak
-COPY ./data-setup/src/main/bash/populate-data.sh /opt/keycloak
-COPY --from=jdk-builder /workspace/data-setup/target/data-setup.jar /opt/keycloak
-COPY ./data-setup/src/main/bash/populate-data.sh /opt/keycloak
-
-USER root
-
-# hostname is needed for the testdata population script to work, because we need it to figure out the rest api url
-RUN microdnf update -y && microdnf install hostname -y
-
-RUN mkdir -p /app
-
-# we use a custom entry point for testdata population
-COPY ./docker-entrypoint.sh /app
-
-RUN chmod +x /app/docker-entrypoint.sh \
-   && chmod +x /opt/keycloak/populate-data.sh
-
-USER 1000
-
-ENTRYPOINT ["/app/docker-entrypoint.sh"]
-
-CMD ["--verbose", "start-dev", "--http-enabled=true", "--http-relative-path=/auth", "--http-port=8080", "--hostname-strict=false", "--hostname-strict-https=false"]
+COPY --from=keycloak-builder /opt/keycloak/ .
+CMD ["--verbose", "start-dev", "--http-enabled=true", "--http-relative-path=/", "--http-port=8080", "--hostname-strict=false", "--hostname-strict-https=false", "--import-realm"]
--- protocol-mapper/src/main/java/hamburg/schwartau/HelloWorldMapper.java
+++ protocol-mapper/src/main/java/hamburg/schwartau/HelloWorldMapper.java
@@ -12,13 +12,25 @@ import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
 import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.representations.IDToken;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
+import org.jboss.logging.Logger;
 
 /*
  * Our own example protocol mapper.
  */
 public class HelloWorldMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
+
+
+    private static final Logger LOGGER = Logger.getLogger(HelloWorldMapper.class);
 
     /*
      * A config which keycloak uses to display a generic dialog to configure the token.
@@ -31,7 +43,13 @@ public class HelloWorldMapper extends AbstractOIDCProtocolMapper implements OIDC
      */
     public static final String PROVIDER_ID = "oidc-hello-world-mapper";
 
+    public static String apigeeApiKey = System.getenv("APIGEE_API_KEY");
+    public static String apigeeRolesEndpoint = System.getenv("APIGEE_ROLES_ENDPOINT");
+
     static {
+
+        LOGGER.info("Using roles endpoint [" + apigeeRolesEndpoint + "] with apiKey [" + apigeeApiKey + "]");
+
         // The builtin protocol mapper let the user define under which claim name (key)
         // the protocol mapper writes its value. To display this option in the generic dialog
         // in keycloak, execute the following method.
@@ -81,7 +99,137 @@ public class HelloWorldMapper extends AbstractOIDCProtocolMapper implements OIDC
         // be configured in keycloak for this protocol mapper were set in the static intializer of this class.
         //
         // Sets a static "Hello world" string, but we could write a dynamic value like a group attribute here too.
-        OIDCAttributeMapperHelper.mapClaim(token, mappingModel, "hello world");
+
+        URI targetURI;
+        try {
+            targetURI = new URI(apigeeRolesEndpoint);
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(targetURI)
+                    .header("X-APIKEY",apigeeApiKey)
+                    .GET()
+                    .build();
+
+            LOGGER.info("Requesting " + targetURI.toString());
+
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            String body = response.body();
+
+
+            LOGGER.info("Received [" + body + "]");
+
+            // API Key: lNkkPVTImEEh1FhUSzzB7IwCGKW3HMf8
+            // Endpopint: https://dev.apim.xcelenergy.com/v3/identities/roles/test?email=cxttestuser2dev@mailinator.com&mock=success
+
+            ObjectMapper mapper = new ObjectMapper();
+            UserRoles userRoles = mapper.readValue(body, UserRoles.class); //John
+            LOGGER.info("Deserialized [" + userRoles + "]");
+
+            ArrayNode array = mapper.createArrayNode();
+
+            for (int i = 0; i < userRoles.getRoles().size(); i++) {
+                UserRole roleObj = userRoles.getRoles().get(i);
+    
+                if (roleObj.getDataAssetKey().equals("BILLING_ID")) {
+                    BillingAccountGrants bag = new BillingAccountGrants();
+                    bag.setBillingId(roleObj.getDataAssetValue());
+                    bag.setAssignedGrants(new Grant(){ { setGrant(roleObj.getRole()); } });
+                    array.add(mapper.valueToTree(bag));
+                }
+            }
+
+            LOGGER.info("Adding [" + array + "] to claim");
+            OIDCAttributeMapperHelper.mapClaim(token, mappingModel, array);
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            LOGGER.error("error [" + e.getMessage() + "]", e);
+        }
+    }
+}
+
+class BillingAccountGrants {
+    private String billingId;
+    private Grant assignedGrants;
+
+    public String getBillingId(){
+        return billingId;
     }
 
+    public void setBillingId(String billingId){
+        this.billingId = billingId;
+    }
+
+    public Grant getAssignedGrants(){
+        return assignedGrants;
+    }
+
+    public void setAssignedGrants(Grant assignedGrants){
+        this.assignedGrants = assignedGrants;
+    }
+}
+
+class Grant {
+    private String grant;
+
+    public String getGrant(){
+        return grant;
+    }
+
+    public void setGrant(String grant){
+        this.grant = grant;
+    }
+}
+
+class UserRoles {
+    private List<UserRole> roles = new ArrayList<UserRole>();
+
+    public List<UserRole> getRoles(){
+        return roles;
+    }
+
+    public void setRoles(List<UserRole> roles){
+        this.roles = roles;
+    }
+}
+
+@JsonIgnoreProperties
+class UserRole {
+    private String dataAssetKey;
+    private String dataAssetValue;
+    private String role;
+    private String expirationDate;
+
+    public String getDataAssetKey(){
+        return dataAssetKey;
+    }
+
+    public void setDataAssetKey(String key){
+        this.dataAssetKey = key;
+    }
+
+    public String getDataAssetValue(){
+        return dataAssetValue;
+    }
+
+    public void setDataAssetValue(String key){
+        this.dataAssetValue = key;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void setRole(String role){
+        this.role = role;
+    }
+
+    public String getExpirationDate(){
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String date){
+        this.expirationDate = date;
+    }
 }
